c9e57bbd0dbc3f37848cc28e6bac899669590f83
Niema-git <elachaouiniema@gmail.com>
2026-03-01 23:00:45 +0000

    la deuxieme implementation de Dao

---
+++ main/java/base/ext/DaoImplV2.java
@@ -0,0 +1,13 @@
+package base.ext;
+
+import base.dao.IDao;
+
+public class DaoImplV2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version capteur");
+        double t=12;
+        return t;
+    }
+}
--- main/java/base/pres/Pres1.java
+++ main/java/base/pres/Pres1.java
@@ -1,12 +1,14 @@
 package base.pres;
 import base.dao.DaoImpl;
 import base.metier.MetierImpl;
+import base.ext.DaoImplV2;
 
 public class Pres1 {
     public static void main(String[] args) {
-        DaoImpl d = new DaoImpl();
-        MetierImpl metier = new MetierImpl();
-        metier.setDao(d);// injection via le setter
+        //DaoImpl d = new DaoImpl();
+        DaoImplV2 d = new DaoImplV2();
+        MetierImpl metier = new MetierImpl(d);
+        //metier.setDao(d);// injection via le setter
         System.out.println("RES="+metier.calcul());
     }
 }
