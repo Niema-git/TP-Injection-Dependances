d9a602a1f39c1d64f4db978d957d4d021d2f070b
Niema-git <elachaouiniema@gmail.com>
2026-03-01 22:50:36 +0000

    la couche presentation n'est pas fermé à la modification

--- main/java/base/pres/Pres1.java
+++ main/java/base/pres/Pres1.java
@@ -5,8 +5,8 @@ import base.metier.MetierImpl;
 public class Pres1 {
     public static void main(String[] args) {
         DaoImpl d = new DaoImpl();
-        MetierImpl metier = new MetierImpl(d);
-        //metier.setDao(dao);// injection via le setter
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(d);// injection via le setter
         System.out.println("RES="+metier.calcul());
     }
 }
